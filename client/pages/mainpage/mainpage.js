82b7add5355ee6e6d42715e5f623a12461d7c11c
xiaolong <904430803@qq.com>
2018-06-29 14:06:46 +0800

    Signed-off-by: xiaolong <904430803@qq.com>

---
+++ client/pages/mainpage/mainpage.js
@@ -0,0 +1,96 @@
+// pages/mainpage/mainpage.js
+var order = ['red', 'yellow', 'blue', 'green', 'red']
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    toView: 'red',
+    scrollTop: 100,
+
+    array: [{
+      message: 'foo',
+    }, {
+      message: 'bar'
+    }]
+
+  },
+  checkboxChange: function (e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+  },
+  
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+
+tap: function (e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
+    }
+  },
+  tapMove: function (e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
+    })
+  }
+
+})
